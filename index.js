af099cb67113990f6ef355bd2792dc450ad482ee
Satyam Rajput <satyamrajput081998@gmail.com>
2024-11-28 07:17:50 -0500

    initil commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+const freecurrencyapi = new Freecurrencyapi('fca_live_4pHk879iZoomcLt2H3DwVpjkSyZWwhTZVrkYbbWw');
+
+convertCurrency('INR','USD',3)
+export async function convertCurrency(fromCurrency,toCurrency,units){
+    const res = await freecurrencyapi.latest({
+        base_currency: fromCurrency,
+        currencies: toCurrency
+    });
+    const multiplier = res.data[toCurrency]
+    return multiplier*units;
+}
